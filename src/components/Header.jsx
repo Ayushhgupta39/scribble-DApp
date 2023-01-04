00e2dc9fa49927905771f6dc31ee331c728520d2
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-01-04 21:54:01 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Scribble</h1>
+      <small>A Decentralized Note making App</small>
+    </header>
+  );
+}
+
+export default Header;
